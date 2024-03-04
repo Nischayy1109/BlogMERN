af217ca96ad57a6c275d4955ac4ffa58d57b70a5
Nischayy1109 <nischayy1109@gmail.com>
2024-03-04 16:33:19 +0530

    setting up react-app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-3xl p-4 text-center text-gray-700'>Blogging site</h1>
+    </>
+  )
+}
+
+export default App
