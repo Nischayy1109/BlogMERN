37c28e424671c80cf38d92c1c3e2e8ca269c6ac4
Nischayy1109 <nischayy1109@gmail.com>
2024-03-06 20:38:54 +0530

    signup and signin completed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import userRoute from './routes/route.js'
+import authRoute from './routes/authroute.js'
 mongoose
     .connect(process.env.MONGO)
     .then(()=>{
@@ -14,6 +15,21 @@ mongoose
 
 const app=express();
 const PORT=8000;
+
+app.use(express.json());
+
+app.use("/api/user",userRoute);
+app.use("/api/auth",authRoute);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||'Internal server error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
 
 app.listen(PORT,()=>{
     console.log(`Server running at port ${PORT} successfully`);
