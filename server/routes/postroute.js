81c651171caca1f4ce506f5d18fd3efc07af8b2c
Nischayy1109 <nischayy1109@gmail.com>
2024-03-17 16:34:04 +0530

    post shown on dashboard

--- server/routes/postroute.js
+++ server/routes/postroute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
 import { errorHandler } from '../utils/errorHandle.js'
-import { create } from '../controllers/post.js';
+import { create,getposts } from '../controllers/post.js';
 
 const router=express.Router();
 
 router.post('/create',verifyToken,create);
+router.get('/getposts',getposts);
 
 export default router
